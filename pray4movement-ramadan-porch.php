8402300265950cba84e24b1ab69945d728b1b992
corsacca <corsacca@gmail.com>
2022-08-18 16:55:37 +0100

    Enable releases

--- pray4movement-ramadan-porch.php
+++ pray4movement-ramadan-porch.php
@@ -196,20 +196,6 @@ if ( !function_exists( "dt_hook_ajax_notice_handler" ) ){
 }
 
 /**
- * Plugin Releases and updates
- * @todo Uncomment and change the url if you want to support remote plugin updating with new versions of your plugin
- * To remove: delete the section of code below and delete the file called version-control.json in the plugin root
- *
- * This section runs the remote plugin updating service, so you can issue distributed updates to your plugin
- *
- * @note See the instructions for version updating to understand the steps involved.
- * @link https://github.com/DiscipleTools/pray4movement-ramadan-porch/wiki/Configuring-Remote-Updating-System
- *
- * @todo Enable this section with your own hosted file
- * @todo An example of this file can be found in (version-control.json)
- * @todo Github is a good option for delivering static json.
- */
-/**
  * Check for plugin updates even when the active theme is not Disciple.Tools
  *
  * Below is the publicly hosted .json file that carries the version information. This file can be hosted
@@ -218,21 +204,21 @@ if ( !function_exists( "dt_hook_ajax_notice_handler" ) ){
  * Also, see the instructions for version updating to understand the steps involved.
  * @see https://github.com/DiscipleTools/disciple-tools-version-control/wiki/How-to-Update-the-Starter-Plugin
  */
-//add_action( 'plugins_loaded', function (){
-//    if ( is_admin() && !( is_multisite() && class_exists( "DT_Multisite" ) ) || wp_doing_cron() ){
-//        // Check for plugin updates
-//        if ( ! class_exists( 'Puc_v4_Factory' ) ) {
-//            if ( file_exists( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' )){
-//                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
-//            }
-//        }
-//        if ( class_exists( 'Puc_v4_Factory' ) ){
-//            Puc_v4_Factory::buildUpdateChecker(
-//                'https://raw.githubusercontent.com/DiscipleTools/pray4movement-ramadan-porch/master/version-control.json',
-//                __FILE__,
-//                'pray4movement-ramadan-porch'
-//            );
-//
-//        }
-//    }
-//} );
+add_action( 'plugins_loaded', function (){
+    if ( is_admin() && !( is_multisite() && class_exists( "DT_Multisite" ) ) || wp_doing_cron() ){
+        // Check for plugin updates
+        if ( ! class_exists( 'Puc_v4_Factory' ) ) {
+            if ( file_exists( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' )){
+                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
+            }
+        }
+        if ( class_exists( 'Puc_v4_Factory' ) ){
+            Puc_v4_Factory::buildUpdateChecker(
+                'https://raw.githubusercontent.com/Pray4Movement/pray4movement-ramadan-porch/master/version-control.json',
+                __FILE__,
+                'pray4movement-ramadan-porch'
+            );
+
+        }
+    }
+} );
